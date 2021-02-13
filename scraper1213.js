75d62df5a87b0c7de03a14dfb03380d883b0a8e7
JichenTan <58278634+JichenTan@users.noreply.github.com>
2021-02-13 20:55:50 +0800

    Add files via upload
    First File

---
+++ scraper1213.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  const url = "https://marketingplatform.google.com/about/partners/find-a-partner?page=1";
+  
+  await page.goto(url);
+
+  const partners = await page.evaluate(() => {
+      Array.from(document.querySelectorAll("div.compact")).map(compact => ({
+        title: compact.querySelector("h3.title").innerText.trim(),
+        logo: compact.querySelector(".logo img").src
+      }))
+
+      return {
+          title,
+          logo
+      }
+  });
+
+  console.log(partners);  
+  await browser.close();
+  
+})();
